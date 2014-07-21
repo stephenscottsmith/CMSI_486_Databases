0b0086caa85ae72ade5321d3bc9a28b6bb3e0b91
Steve <steve@tgiltd.com>
2014-07-20 17:55:42 -0700

    Refactored creation of directories code

--- Flat File System/Flava (Flatty Java)/Flava.java
+++ Flat File System/Flava (Flatty Java)/Flava.java
@@ -12,13 +12,7 @@ public class Flava {
 	private static String currentDatabase = "test";
 
 	public static void main(String[] args) {
-
-
-		// Prepare the list of databases to use
-		// String [] databases = getListOfDatabases();
-		System.out.println(currentDatabase);
-		currentDatabase = "bash";
-		System.out.println(currentDatabase);
+		initiateStartup();
 
 		// Cite: http://stackoverflow.com/questions/12396765/continuous-input-commands
 		Scanner sc = new Scanner(System.in);
@@ -43,33 +37,87 @@ public class Flava {
 		        }
 		    } else if (commands.length >= 2) {
 		        // Handle the parameters parameters
-		    	String flatCommand = commands[0].toLowerCase();
+		    	// String flatCommand = commands[0].toLowerCase();
 
-		    	switch (flatCommand) {
-		    		case "create" :
-		    			// Can CREATE the following:
-		    			// Datebase
-		    			// Table
+		    	// switch (flatCommand) {
+		    	// 	case "create" :
+		    	// 		// Can CREATE the following:
+		    	// 		// Datebase
+		    	// 		// Table
 
-		    			create(Arrays.copyOfRange(commands, 1, commands.length));
+		    	// 		create(Arrays.copyOfRange(commands, 1, commands.length));
 		    			
-		    			break;
-		    		case "delete" :
+		    	// 		break;
+		    	// 	case "delete" :
 		  
-		    			break;
-		    		case "update" :
+		    	// 		break;
+		    	// 	case "update" :
 		  
-		    			break;
-		    		case "select" :
+		    	// 		break;
+		    	// 	case "select" :
 		  
-		    			break;
-		    		case "insert" :
+		    	// 		break;
+		    	// 	case "insert" :
 		  
-		    			break;
-		    		default:
-		    			invalidCommand();
-		    	}
+		    	// 		break;
+		    	// 	default:
+		    	// 		invalidCommand();
+		    	// }
 		    } 
+		}
+	}
+
+	public static void initiateStartup () {
+		// 1. Find data folder
+		// 	a. If data folder exists:
+		// 		-Populate global list of databases w/ string names
+		// 		-Confirm that test database exists
+		// 	b. If it doesn't exist, create it and create test database within it
+		// c. Set global database to test
+		String [] databases;
+		if (directoryExists("data")) {
+
+		} else {
+			createDirectory("data");
+			createDirectory("./data/test.fdf");
+		}
+
+	}
+
+	public static boolean directoryExists (String path) {
+		File directory = new File(path);
+		return directory.exists();
+	}
+
+	public static void createDatabase (String databaseName) {
+		createDirectory("./data/" + databaseName + ".fdb");
+	}
+
+	public static void createTable (String databaseName, String tableName) {
+		createDirectory(("./data/" + databaseName + ".fdb/" +  tableName), false);
+	}
+
+	public static void createDirectory (String directoryPath, Boolean isDatabase) {
+		// Cite: http://stackoverflow.com/questions/3634853/how-to-create-a-directory-in-java
+		File newDir = new File(databaseName);
+
+		if (!newDir.exists()) {
+		    System.out.println("Attempting to create database: " + databaseName + "...");
+		    boolean createDBResult = false;
+
+		    try {
+		        newDir.mkdir();
+		        createDBResult = true;
+		    } catch (SecurityException se) {
+		        //handle it
+		        throw new SecurityException("You may not have permissions to create a new database! Consult your IT.");
+		    }        
+		    if (createDBResult) {    
+		       System.out.println("Database " + databaseName + " has been successfully created.");  
+		    } 
+
+		} else {
+		    System.out.println("The " + databaseName + " database already exists! Try using another name.");
 		}
 	}
 
@@ -81,75 +129,28 @@ public class Flava {
 		System.out.println("THE COMMAND YOU ENTERED CANNOT BE PARSED!");
 	}
 
-	public static void create(String [] commands) {
-		String objectToCreate = commands[0].toLowerCase();
+	// public static void create(String [] commands) {
+	// 	String objectToCreate = commands[0].toLowerCase();
 
-		switch (objectToCreate) {
-			case "database" :
-				System.out.println("DATABASE");
-				createDatabase(Arrays.copyOfRange(commands, 1, commands.length));
-				break;
-			case "table" :
-				System.out.println("TABLE");
-				break;
-			case "index" :
-				System.out.println("INDEX");
-				break;
-			default :
-				System.out.println("THE OBJECT YOU ARE TRYING TO CREATE IS NOT A VALID OBJECT!");
-				break;
-		}
+	// 	switch (objectToCreate) {
+	// 		case "database" :
+	// 			System.out.println("DATABASE");
+	// 			createDatabase(Arrays.copyOfRange(commands, 1, commands.length));
+	// 			break;
+	// 		case "table" :
+	// 			System.out.println("TABLE");
+	// 			break;
+	// 		case "index" :
+	// 			System.out.println("INDEX");
+	// 			break;
+	// 		default :
+	// 			System.out.println("THE OBJECT YOU ARE TRYING TO CREATE IS NOT A VALID OBJECT!");
+	// 			break;
+	// 	}
 
-	}
+	// }
 
-	public static void createDatabase(String [] databaseName) {
-		if (databaseName.length == 1) {
-			// Cite: http://stackoverflow.com/questions/3634853/how-to-create-a-directory-in-java
-			File newDir = new File(databaseName[0]);
-			if (!newDir.exists()) {
-			    System.out.println("Attempting to create database: " + databaseName[0] + "...");
-			    boolean createDBResult = false;
 
-			    try {
-			        newDir.mkdir();
-			        createDBResult = true;
-			    } catch (SecurityException se){
-			        //handle it
-			        throw new SecurityException("You may not have permissions to create a new database! Consult your IT.");
-			    }        
-			    if (createDBResult) {    
-			       System.out.println("Database " + databaseName[0] + " has been successfully created.");  
-			    } 
-
-			} else {
-			    System.out.println("The " + databaseName[0] + " database already exists! Try using another name.");
-			}
-		} else {
-			invalidCommand();
-		}
-	}
-
-	public static void createTable (String [] commandTokens) {
-
-		String newTablePath = "./" + commandTokens[0] + "/" + commandTokens[1];
-		File newTable = new File(newTablePath);
-		
-		if (!newTable.exists()) {
-			try {
-				
-				newTable.createNewFile();
-			} catch (IOException io) {
-			
-			}
-
-			System.out.println("The " + commandTokens[0]+ " has successfully been created on the " + commandTokens[0] + ".\n" +
-							   "Path: " + newTablePath);
-
-		} else {
-			System.out.println("The " + commandTokens[0] + " Table already exists in this database! \n" + 
-							   "Try using another name for the Table you wish to create.");
-		}
-	}
 
 }
 
