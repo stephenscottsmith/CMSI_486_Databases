4c5dbff36b34adef9a135c9bd24f972ade0cdef8
Steve <steve@tgiltd.com>
2014-09-09 07:21:00 -0700

    Week 2 and 3 MongoDB hw

--- MongoDB University/Node/Week 1/helloworld.js
+++ MongoDB University/Node/Week 1/helloworld.js
@@ -1,5 +1,7 @@
-var http = require('http');
-
+var http = require('http'),
+	express = require('express'),
+	cons = requires('consolidate'),
+	mongodb = require('mongodb');
 
 var server = http.createServer(function (request, response) {
 	response.writeHead(200, {"Content-Type":"text/plain"});
